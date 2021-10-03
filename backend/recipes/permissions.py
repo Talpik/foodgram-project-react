753afa8f1bb97828b3ee2c4eeb858b5cbf7141a0
Talpa <talpa1989@gmail.com>
2021-10-03 15:59:55 +0300

    added doc-strings in permissions.py recipes app.

--- backend/recipes/permissions.py
+++ backend/recipes/permissions.py
@@ -2,12 +2,26 @@ from rest_framework.permissions import BasePermission
 
 
 class IsOwnerOrAdminOrReadOnly(BasePermission):
+    """
+    Class for checking the permissions of users
+    to execute certain requests to the API.
+    """
     def has_permission(self, request, view):
+        """
+        Only authenticated users are allowed to post requests.
+
+        """
         if request.method == 'POST':
             return request.user.is_authenticated
         return True
 
     def has_object_permission(self, request, view, obj):
+        """
+        The method allows the execution of queries to users -
+        who are the author of these objects,
+        or this user is a superuser, or these are safe methods.
+
+        """
         if request.method in ('GET', 'HEAD', 'OPTIONS') or request.user.is_superuser:
             return True
         return request.user == obj.author
