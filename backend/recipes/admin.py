a8cfd83e61b492490b4ed7f2113c3fe47b50b182
Talpa <talpa1989@gmail.com>
2021-10-08 09:43:41 +0300

    import groups by PEP8

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.contrib.admin.decorators import register
+
 from .models import (ProductCategory, Ingredient, RecipeIngredient,
                      Recipe, Tag)
 
